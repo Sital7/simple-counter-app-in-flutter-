e9ef2a149b80cbfb47f53d34cea92ee22a12f2d9
Sital khatri <129025536+Sital7@users.noreply.github.com>
2024-12-23 06:31:05 -0800

    Add files via upload
    this main.dart file is 2nd counter application

--- main.dart
+++ main.dart
@@ -6,49 +6,83 @@ class CounterApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Counter App',
-      debugShowCheckedModeBanner: false, // Removes debug banner
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: CounterHome(),
+      title: 'Flutter Counter Application',
+      home: CounterScreen(),
     );
   }
 }
 
-class CounterHome extends StatefulWidget {
+class CounterScreen extends StatefulWidget {
   @override
-  _CounterHomeState createState() => _CounterHomeState();
+  CounterScreenState createState() => CounterScreenState();
 }
 
-class _CounterHomeState extends State<CounterHome> {
-  int _counter = 0;
+class CounterScreenState extends State<CounterScreen> {
+  int _count = 0;
 
-  void _incrementCounter() {
+  void increment() {
     setState(() {
-      _counter++;
+      _count++;
+    });
+  }
+
+  void decrement() {
+    if (_count < 1) {
+      return;
+    }
+    setState(() {
+      _count--;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Simple Counter App')),
+      appBar: AppBar(
+        title: Text('Counter Screen'),
+      ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('You have pressed the button this many times:'),
-            Text(
-              '$_counter',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          Text(
+          'Count Value:',
+          style: TextStyle(fontSize: 23),
+        ),
+        Text(
+          '$_count',
+          style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: increment,
+              child: Text('Increment'),
+            ),
+            SizedBox(width: 18),
+            ElevatedButton(
+              onPressed: decrement,
+              child: Text('Decrement'),
             ),
           ],
+          //   FloatingActionButton(
+          //     child: Icon(Icons.add), // Correct Icon for addition
+          //     onPressed: increment,
+          //   ),
+          //   Text("Count"
+          //     // 'Count: $_count',
+          //     // style: TextStyle(fontSize: 24),
+          //   ),
+          //   FloatingActionButton(
+          //     child: Icon(Icons.remove), // Correct Icon for subtraction
+          //     onPressed: decrement,
+          //   ),
+          // ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      ],
+    )
+      )
     );
   }
 }
